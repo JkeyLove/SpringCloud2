0e0ee594a34c54e66bf98a39618e036f6621fc81
JkeyLove <1826067520@qq.com>
2023-06-20 20:54:19 +0800

    Nacos:配置中心(user-service)

--- user-service/src/main/java/org/example/controller/UserController.java
+++ user-service/src/main/java/org/example/controller/UserController.java
@@ -2,6 +2,8 @@ package org.example.controller;
 
 import org.example.entity.User;
 import org.example.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 @RestController
+@RefreshScope       //配置文件的热更新,添加此注解就能实现自动刷新了
 public class UserController {
 
     @Resource
     UserService service;
 
+    @Value("${test.lbwnb}")
+    String test;
+
     //这里以RESTFul风格为例
     @RequestMapping("/user/{uid}")
     public User findUserById(@PathVariable("uid") int uid){
-        System.out.println("user被调用了");
+        System.out.println(test);
         return service.getUserById(uid);
     }
 }
